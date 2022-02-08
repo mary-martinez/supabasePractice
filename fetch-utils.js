3b4eecf9a8f0e59c6074a823391f00a8af63282b
Mary Martinez <mary.diana.martinez@gmail.com>
2022-02-08 12:49:40 -0800

    initial effort with supabase

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,43 +1,57 @@
-const SUPABASE_URL = "https://njovlynmvtgkoexzepbb.supabase.co";
+const SUPABASE_URL = "https://nxkfcrgevcuksvdnaugc.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTMxNjU3MSwiZXhwIjoxOTU2ODkyNTcxfQ.4sFYy_xhbdh7hVgaEmH_ILGTIl6rsqtUgzAJsizIxkA";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54a2ZjcmdldmN1a3N2ZG5hdWdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE0NjAsImV4cCI6MTk1OTkxNzQ2MH0.9e3rnFUxMChSkwzJQcBImTDSwY_4VDtfmLDUZ-dY7gc";
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 function checkError({ data, error }) {
-  return error ? console.error(error) : data;
+    return error ? console.error(error) : data;
 }
 
 export async function getMovies() {
-  // return the list of all movies
-  const resp = await client.from("movies").select("*");
-  return checkError(resp);
+    // return the list of all movies
+    const resp = await client.from("movies").select("*");
+    // console.log(resp);
+    return checkError(resp);
 }
 
 export async function getMoviesWithDirector() {
-  // return the list of all the movies with their director
+    // return the list of all the movies with their director
+    const resp = await client.from('movies').select(`title, directors (name)`);
+    // console.log(resp);
+    return checkError(resp);
 }
 
 export async function getDirectorNames() {
-  // return the list of the director's names
+    // return the list of the director's names
+    const resp = await client.from('directors').select('name');
+    // console.log(resp);
+    return checkError(resp);
 }
 
 export async function getMovieById(id) {
-  // return the movie with the given id
+    // return the movie with the given id
+    // const resp = await client.from('movies').select(`id, title`);
+    // const resp = await client.from('movies').select(`id, title`);
+    // const title = resp[id];
+    // console.log(title);
+    const resp = await client.from('movies').select('title').eq('id', id).single();
+    console.log(resp);
+    return checkError(resp);
 }
 
 export async function getMovieByTitle(title) {
-  // return the movie with the given title
+    // return the movie with the given title
 }
 
 export async function getOldestMovie() {
-  // return the oldest movie (assume the database is not sorted)
+    // return the oldest movie (assume the database is not sorted)
 }
 
 export async function getMoviesAfter(year) {
-  // return movies made after the year passed in
+    // return movies made after the year passed in
 }
 
 export async function getHighestGrossingMovie() {
-  // return movie with the highest box office total
+    // return movie with the highest box office total
 }
